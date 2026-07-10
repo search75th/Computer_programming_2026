645eace21d35aa3354b688bb16b1b7f4dd3a858d
search75th <search4399th@gmail.com>
2026-07-10 03:06:23 +0000

    code_test_v3

---
+++ WEEK_1/code_test.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    //printf("intouch puengphraphorn \n") ;
+    //printf("69011347 \n");
+    //printf("%s\n","KMITL");
+    //printf("%f\n",9.9);
+    //printf("%d\n",9);
+    //printf("%c\n",'A');
+    printf("intouch puengphraphorn \n" "69011347 \n" "%s\n %f\n %d\n %c\n", "KMITL" , 9.9 , 9 ,'A');
+
+    return 0 ;
+}
---
+++ WEEK_1/code_test_1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    printf("Subject : %s(%d)\n", "Programming" , 2026);
+    printf("Point : %d\nGrade : %c",99,'A');
+    printf("\n");
+    return 0 ;
+}
