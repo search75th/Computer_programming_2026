c4eda2d5e362f344ef3a078d91b01db113ccb6cc
search75th <search4399th@gmail.com>
2026-07-10 03:24:10 +0000

    code_test_V4

---
+++ WEEK_1/code_test_2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+
+{
+    int a, b, c ;
+    printf("enter the first value :");
+    scanf("%d", &a);
+    printf("enter the second value :");
+    scanf("%d", &b);
+    c = a + b ;
+    printf("%d + %d = %d\n", a , b , c);
+    
+    return 0 ;
+
+}
---
+++ WEEK_1/code_test_3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    printf("Programming :%c\n   ", 'A');
+    printf("Programming :%s\n   ", "B+");
+    printf("Programming :%s\n   ", "B+");
+    printf("Programming :%c\n   ", 'B');
+    printf("Programming :%f\n   ", 3.5);
+
+    return 0 ;
+}
