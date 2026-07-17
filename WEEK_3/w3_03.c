d3a76193026d0622ce5c6f6126043895f793a511
search75th <search4399th@gmail.com>
2026-07-17 04:29:42 +0000

    code_test_V5

---
+++ WEEK_3/w3_03.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+
+{
+  float point;
+  char name[20];
+  int x = 10 , y ;
+  printf("Before : %d\n" , x);
+  y = 5+x ;
+  printf("After : x = %d , y = %d\n" , x,y);
+
+  return 0;
+}
