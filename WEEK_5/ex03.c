fa23c530d1a3ea988584c5efab493080071962a7
search75th <search4399th@gmail.com>
2026-08-07 04:42:37 +0000

    code_test_V5

---
+++ WEEK_5/ex03.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int matrix[3][3];
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("Enter numbers [%d][%d] : ", i, j);
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    printf("***Matrix***\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d\t", matrix[i][j]);
+        }
+        printf("\n\n");
+    }
+
+    return 0;
+}
